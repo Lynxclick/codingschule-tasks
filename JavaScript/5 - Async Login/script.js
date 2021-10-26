ee604945b0138b069c96e115d62203b29587fe06
Rocketboerd <91976244+Rocketboerd@users.noreply.github.com>
2021-10-26 22:25:49 +0200

    worked in task "Asynchrone Programmierung"

--- JavaScript/5 - Async Login/script.js
+++ JavaScript/5 - Async Login/script.js
@@ -1,3 +1,4 @@
+/* 
 function loginUser(username, password) {
     return new Promise((resolve,reject) => {
         setTimeout ( () => {
@@ -17,3 +18,37 @@ function getUserDetails(userEmail) {
 loginUser("Codingschule", "123456")
 .then ( user => getUserDetails(user.userEmail))
 .then ( profile => console.log(profile))
+ */
+
+
+
+
+  async function getUserDetails(userEmail) {
+
+    let promise = new Promise((resolve, reject) => {
+      setTimeout(() => 
+        resolve({userPostcode : "40476"}), 1500);
+    });
+  
+    let user = await promise;
+  
+    getUserDetails(user.userEmail);
+  }
+
+  
+  
+  async function loginUser(username, password) {
+
+    let promise = new Promise((resolve, reject) => {
+      setTimeout(() => 
+        resolve({userEmail : "kontakt@codingschule.de"}), 1500);
+    });
+  
+    let profile = await promise;
+  
+    console.log(profile);
+  }
+
+
+loginUser("Codingschule", "123456");
+  
