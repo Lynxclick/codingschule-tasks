fb2908a670ff8cb3859ce056b4b44af037957ed8
Rocketboerd <91976244+Rocketboerd@users.noreply.github.com>
2021-11-01 22:38:46 +0100

    finish task "weather app"

--- JavaScript/6 - Wetterapp/script.js
+++ JavaScript/6 - Wetterapp/script.js
@@ -1,12 +1,25 @@
 const city = encodeURI("Wernigerode") // Anpassen für die eigene Stadt
 
+
 document.querySelector(".city").innerText = city;
 
 
 async function weatherUpdate() {
     const result = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Wernigerode&appid=868bc208df0d7d2c94f433afb09be393")
     const data = await result.json()
+
+    let tempValue = data["main"]["temp"]
+    document.querySelector(".temperature").innerText = Math.round(tempValue-273.15);
+
+    let timeValue = new Date(); /*data["sys"]["timezone"]*/
+    document.querySelector(".time").innerText = timeValue;
+    setTimeout(weatherUpdate, 10000);
+
+    let imageValue = data["weather"]["0"]["icon"]
+    document.querySelector(".image").innerHTML = imageValue;
+   
     console.log(data)
+    // console.log(Intl.DateTimeFormat().resolvedOptions().timeValue)
 }
 
 weatherUpdate();
