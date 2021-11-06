b86d8ef7a0ab48fef8b45a1f35c1ed9931b58478
Rocketboerd <91976244+Rocketboerd@users.noreply.github.com>
2021-11-06 14:08:43 +0100

    add new task "my App"

---
+++ myapp/src/App.js
@@ -0,0 +1,11 @@
+import './design.css';
+
+//const element = <h1 className="test">Überschrift</h1>;
+
+
+function App() {
+  return (<h1 className="test">Überschrift</h1>);
+  //document.getElementsBy("Überschrift").className = "test;"
+}
+
+export default App;
