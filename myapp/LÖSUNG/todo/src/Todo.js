2314e6d873f3cc6a84bd9320d1719bdc19f90392
Rocketboerd <91976244+Rocketboerd@users.noreply.github.com>
2021-11-09 21:17:49 +0100

    add "LÖSUNG myapp"

---
+++ myapp/LÖSUNG/todo/src/Todo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './my.css';
+
+function Todo() {
+    return (
+        <div className="todo">
+            Einkaufen
+        </div>
+    )
+}
+
+export default Todo
