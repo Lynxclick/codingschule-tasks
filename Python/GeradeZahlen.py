d261ac4ce0de5cce945a6d5eff9791e779d4d10d
Rocketboerd <91976244+Rocketboerd@users.noreply.github.com>
2021-12-16 21:26:32 +0100

    add Django

--- Python/GeradeZahlen.py
+++ Python/GeradeZahlen.py
@@ -1,1 +1,21 @@
-Obergrenze = input("Nenne eine Obergrenze = ")
+Obergrenze = int(input("Nenne eine Obergrenze = "))
+
+
+#try:
+#    int(Obergrenze)
+#    it_is = True
+#except ValueError:
+#    it_is = False
+
+print (Obergrenze)
+
+Zahlenfolge = range(Obergrenze)
+    
+for Zahl in Zahlenfolge:
+    print(Zahl)
+
+if Zahl % 2 == 0:
+    print("Die Zahl " + Zahl + " ist gerade")
+else:
+    print("Die Zahl " + Zahl + " ist ungerade")
+
