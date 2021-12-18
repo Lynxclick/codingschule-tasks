1a382c61b407273bd2e40938a67c61fa97ada1ea
Rocketboerd <91976244+Rocketboerd@users.noreply.github.com>
2021-12-18 14:17:35 +0100

    Datenbank

--- Python/api/views.py
+++ Python/api/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
-
-from django.http import HttpResponse
+from .models import Customer
+from .models import Post
+#from api.models import Expense
+#from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+
+    # context = {
+    #     "posts": [
+    #         {"id": 1, "author": "Ada Lovelace", "text": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores"},
+    #         {"id": 2, "author": "Bill Gates", "text": "Der Computer wurde zur Lösung von Problemen erfunden, die es früher nicht gab."}
+    #     ]
+    # }
     
-    return HttpResponse("Der erste Satz")
+    customers = Customer.objects.all()
+    context = {
+        "customer" : customers
+    }
+
+    posts = Post.objects.all()
+    context = {
+        "posts" : posts
+    }
+
+
+    return render(request, "api/index.html", context)
 
 
--- Python/backend/urls.py
+++ Python/backend/urls.py
@@ -21,4 +21,5 @@ from api import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+    #path('', include('tutorial.urls')),
 ]
