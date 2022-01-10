8096b40b9f3c5358882704e412328e15c67b4fd5
Kane <91976244+Rocketboerd@users.noreply.github.com>
2022-01-10 22:44:41 +0100

    delete

--- Python/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from api import views
-
-urlpatterns = [
-    path('', views.index),
-    path('api/posts', views.api_get),
-    path('new/<str:name>/', views.api_new),
-    path('api2/posts', views.api_view),
-]
