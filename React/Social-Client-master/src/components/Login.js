f1ff00b17f5203335819a53d2839cd49fcdc529c
Rocketboerd <91976244+Rocketboerd@users.noreply.github.com>
2022-01-18 21:21:49 +0100

    update app frontend

--- React/Social-Client-master/src/components/Login.js
+++ React/Social-Client-master/src/components/Login.js
@@ -1,17 +1,32 @@
-import React, {useState} from 'react'
+import React, {useState, setState} from 'react'
 import styled from 'styled-components'
 
 function Login({setUser}) {
     const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
 
-    function handleLogin(e) {
-        console.log(username)
+    async function handleLogin(e) {
         setUser(username)
+
+        const result = await fetch('http://127.0.0.1:8000/api/token/', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+            "username": "Kane", "password": "Testpasswort"
+            })
+        })
+        const data = await result.json()
+        console.log(data)
+
+        //postToken(data)
     }
 
     return (
         <LoginContainer>
             <Input placeholder='Benutzername' value={username} onChange={(e) => setUsername(e.target.value)}></Input>
+            <Input placeholder='Passwort' value={password} onChange={(e) => setPassword(e.target.value)}></Input>
             <Button onClick={handleLogin}>Rein da!</Button>
         </LoginContainer>
     )
@@ -20,11 +35,11 @@ function Login({setUser}) {
 export default Login
 
 const LoginContainer = styled.div`
-grid-area: Main;
-padding: 10px;
-display: flex;
-flex-direction: column;
-align-items: center;
+    grid-area: Main;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `
 
 const Button = styled.div`
